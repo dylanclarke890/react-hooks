0463403839650da4e722f986938865214d4ecddf
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-30 21:31:55 +0100

    useDefaultValue tidy up

--- src/hooks/useDefaultValue.js
+++ src/hooks/useDefaultValue.js
@@ -4,16 +4,15 @@ import { defaultVal } from "../utils";
 /**
  * Hook for default values.
  * @param {*} [defaultValue] A default value.
- * @param {*} [valueIfDefaultValueIsUndefined] A value to use if the given default value is undefined.
- * @return {Array} A tuple with initially the default value at index 0, as well as a callback to set
- *                 the value at index 1.
+ * @param {*} [fallbackValue] A value to use if the given default value is
+ * undefined.
+ * @return {Array} A tuple with initially the default value at index 0, as well as a callback
+ * to set the value at index 1.
  */
 export default function useDefaultValue(
   defaultValue = void 0,
-  valueIfDefaultValueIsUndefined = void 0
+  fallbackValue = void 0
 ) {
-  const [value, setValue] = useState(
-    defaultVal(defaultValue, valueIfDefaultValueIsUndefined)
-  );
+  const [value, setValue] = useState(defaultVal(defaultValue, fallbackValue));
   return [value, setValue];
 }
