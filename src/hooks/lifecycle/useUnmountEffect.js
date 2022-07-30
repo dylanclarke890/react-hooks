6e4a58d47e03891235eb1051fa24a3b0040096dd
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-30 21:04:45 +0100

    useUmountEffect hook

---
+++ src/hooks/lifecycle/useUnmountEffect.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import useCallbackRef from "./useCallbackRef";
+
+/**
+ * Hook to execute a callback on unmount.
+ * @param {Function} func The callback to execute.
+ */
+export default function useUnmountEffect(func) {
+  const funcRef = useCallbackRef(func);
+  useEffect(() => () => funcRef.current(), [funcRef]);
+}
