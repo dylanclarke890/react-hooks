9ef458815a84c28637624a07da73705ccf7abbb5
dylanclarke890 <dylanclarke890@gmail.com>
2022-07-31 17:34:26 +0100

    useIsUpdate

---
+++ src/hooks/component-lifecycle/useIsUpdate.js
@@ -0,0 +1,9 @@
+import usePrevious from "../usePrevious";
+
+/**
+ * Hook to tell if the current rendering is due to an update or not.
+ * @return {boolean} True if the current rendering is due to an update, false otherwise.
+ */
+export default function useIsUpdate() {
+  return !!usePrevious(true);
+}
