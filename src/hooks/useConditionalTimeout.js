67b31be49d15d3b4258541c0b9fd03afe348a212
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-02 22:51:19 +0100

    added useConditionalTimeout

---
+++ src/hooks/useConditionalTimeout.js
@@ -0,0 +1,69 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import usePrevious from "./usePrevious";
+
+const defaultOptions = {
+  cancelOnUnmount: true,
+  cancelOnConditionChange: true,
+};
+
+/**
+ * An async-utility hook that accepts a callback function and a delay time (in milliseconds), then delays the
+ * execution of the given function by the defined time from when the condition verifies.
+ */
+export default function useConditionalTimeout(
+  fn,
+  milliseconds,
+  condition,
+  options = defaultOptions
+) {
+  const opts = { ...defaultOptions, ...(options || {}) };
+  const timeout = useRef();
+  const callback = useRef(fn);
+  const [isCleared, setIsCleared] = useState(false);
+  const prevCondition = usePrevious(condition);
+
+  // the clear method
+  const clear = useCallback(() => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+      setIsCleared(true);
+    }
+  }, []);
+
+  // if the provided function changes, change its reference
+  useEffect(() => {
+    if (typeof fn === "function") {
+      callback.current = fn;
+    }
+  }, [fn]);
+
+  // when the milliseconds change, reset the timeout
+  useEffect(() => {
+    if (condition && typeof milliseconds === "number") {
+      timeout.current = setTimeout(() => {
+        callback.current();
+      }, milliseconds);
+    }
+  }, [condition, milliseconds]);
+
+  // when the condition change, clear the timeout
+  useEffect(() => {
+    if (
+      prevCondition &&
+      condition !== prevCondition &&
+      opts.cancelOnConditionChange
+    ) {
+      clear();
+    }
+  }, [condition, options]);
+
+  // when component unmount clear the timeout
+  useEffect(
+    () => () => {
+      if (opts.cancelOnUnmount) clear();
+    },
+    []
+  );
+
+  return [isCleared, clear];
+}
