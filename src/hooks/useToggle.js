c78383517825a62c100bebe38767191f0d68f4dd
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-02 21:57:42 +0100

    added useToggle

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+/**
+ * Custom hook which returns a boolean state and a state toggle function.
+ * @param {boolean} initialValue Initial value of the state.
+ * @returns {Array} Array containing boolean state and a state toggle function.
+ */
+export default function useToggle(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  const toggleValue = () => {
+    setValue((currentValue) => !currentValue);
+  };
+  return [value, toggleValue];
+}
