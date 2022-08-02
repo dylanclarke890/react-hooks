f0c2ef4457111bae6ffa106514caf9b5ddb3cc80
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-02 18:27:38 +0100

    added useArray

---
+++ src/hooks/useArray.js
@@ -0,0 +1,22 @@
+import useFactory from "./useFactory";
+import ImmutableLinkedOrderedMap from "dc-javascript-utils";
+
+/**
+ * Hook returning an array for a given a data structure.
+ * @param {Array|ImmutableLinkedOrderedMap} dataStructure A data structure.
+ * @return {Array} An array containing the values of the data structure.
+ * If an array is given as the data structure, the same array will be returned by this hook.
+ */
+export default function useArray(dataStructure) {
+  const arrayFactoryFn = useFactory(
+    () => [
+      [
+        ImmutableLinkedOrderedMap.isMap(dataStructure),
+        () => dataStructure.values(),
+      ],
+      () => dataStructure,
+    ],
+    [dataStructure]
+  );
+  return arrayFactoryFn();
+}
