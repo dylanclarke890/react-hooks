c32214a197222a97472fc2be7061aef4edf22d66
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-02 20:44:02 +0100

    added useAwaitableStateWithLayoutEffect

---
+++ src/hooks/useAwaitableStateWithLayoutEffect.js
@@ -0,0 +1,17 @@
+import { useAwaitableState } from "./useAwaitableStateWithEffect";
+import useStateWithLayoutEffectCallback from "./useStateWithLayoutEffectCallback";
+
+/**
+ * Hook to use a state with a `setState` function which can be awaited until the state change
+ * is performed and the underlying layout effect (`useLayoutEffect`) is executed.
+ * @param {*|Function} initialState The initial state or a lazy callback returning the initial state.
+ * @return {Array} A tuple of two values, current state and callback to set state, like the one
+ * returned by the "useState" hook. The callback to set state may be awaited until the state
+ * change is performed and the underlying layout effect (`useLayoutEffect`) is executed.
+ */
+export default function useAwaitableStateWithLayoutEffect(initialState) {
+  return useAwaitableState({
+    initialState,
+    useStateWithSetStateCallback: useStateWithLayoutEffectCallback,
+  });
+}
