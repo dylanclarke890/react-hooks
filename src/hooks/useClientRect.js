9745db3877b733589e30d976e1462f180a2020e4
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-02 19:43:13 +0100

    added useClientRect

---
+++ src/hooks/useClientRect.js
@@ -0,0 +1,19 @@
+import { useState, useCallback } from "react";
+
+/**
+ * Hook returning a ref callback to measure the dimensions of a DOM element and the current
+ * dimensions of the measured element.
+ * @see https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+ * @return {Array} An array containing the ref callback at index 0 and the dimensions
+ * (an object with a "width" and "height" property) of the last measurement of the element
+ * at index 1.
+ */
+export default function useClientRect() {
+  const [rect, setRect] = useState(null);
+  const ref = useCallback((node) => {
+    if (node !== null) {
+      setRect(node.getBoundingClientRect());
+    }
+  }, []);
+  return [ref, rect];
+}
