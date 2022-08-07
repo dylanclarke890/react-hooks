de57b35abe252a44cf38922209e8e49a186b118a
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-08 00:51:19 +0100

    added tests for usePrevious

---
+++ src/__tests__/hooks/usePrevious.test.js
@@ -0,0 +1,22 @@
+import { renderHook } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import usePrevious from "../../hooks/usePrevious";
+
+describe("usePrevious", () => {
+  test("Should return undefined for the initial state", () => {
+    const { result } = renderHook(() => usePrevious(1));
+    expect(result.current).toBeUndefined();
+  });
+
+  test("Should return the previous state on state change", () => {
+    let initial = 1;
+    const { result, rerender } = renderHook(() => usePrevious(initial));
+    expect(result.current).toBeUndefined();
+    initial = 5;
+    rerender();
+    expect(result.current).toBe(1);
+    initial = 10;
+    rerender();
+    expect(result.current).toBe(5);
+  });
+});
