80d458cfa0ca67da526c59b08bde830319b12bd0
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-08 22:23:06 +0100

    added test for useMountEffect

--- src/__tests__/hooks/component-lifecycle/useMountEffect.test.js
+++ src/__tests__/hooks/component-lifecycle/useMountEffect.test.js
@@ -1,1 +1,15 @@
-test.todo("useMountEffect");
+import useMountEffect from "../../../hooks/component-lifecycle/useMountEffect";
+import { renderHook } from "@testing-library/react-hooks";
+
+describe("useMountEffect", () => {
+  test("should only run on mount", () => {
+    const mock = jest.fn();
+    const { rerender } = renderHook(() => useMountEffect(mock));
+    expect(mock).toBeCalledTimes(1);
+
+    for (let i = 0; i < 5; i++) {
+      rerender();
+      expect(mock).toBeCalledTimes(1);
+    }
+  });
+});
