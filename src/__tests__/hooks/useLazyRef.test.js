cb1481df7fed40d7b6d7e843add48c27e3ed7788
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 00:31:46 +0100

    added useLazyRef tests

--- src/__tests__/hooks/useLazyRef.test.js
+++ src/__tests__/hooks/useLazyRef.test.js
@@ -1,1 +1,13 @@
-test.todo("useLazyRef");
+import { renderHook } from "@testing-library/react";
+import useLazyRef from "../../hooks/useLazyRef";
+
+describe("useLazyRef", () => {
+  const setUp = (fn) => renderHook(() => useLazyRef(fn));
+
+  test("returns a func on mount which returns state lazily", () => {
+    const mockFn = jest.fn(() => () => true);
+    const { result } = setUp(mockFn);
+    expect(mockFn).toBeCalled();
+    expect(result.current.current()).toBe(true);
+  });
+});
