00814b8b92c54eb0cb34b344bc8143b4177c9e85
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-08 22:58:40 +0100

    added useForceUpdate test

--- src/__tests__/hooks/component-lifecycle/useForceUpdate.test.js
+++ src/__tests__/hooks/component-lifecycle/useForceUpdate.test.js
@@ -1,1 +1,21 @@
-test.todo("useForceUpdate");
+import useForceUpdate from "../../../hooks/component-lifecycle/useForceUpdate";
+import { fireEvent, render } from "@testing-library/react";
+describe("useForceUpdate", () => {
+  test("rerenders the component when called", () => {
+    const renderSpy = jest.fn();
+    function Component() {
+      const forceUpdate = useForceUpdate();
+      renderSpy();
+      return (
+        <button data-testid="btn" onClick={forceUpdate}>
+          Click me
+        </button>
+      );
+    }
+    const { getByTestId } = render(<Component />);
+    for (let i = 1; i < 10; i++) {
+      expect(renderSpy).toHaveBeenCalledTimes(i);
+      fireEvent.click(getByTestId("btn"));
+    }
+  });
+});
