0015c085d6f7c19f98eb18b5a83486a5a4e26400
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 00:03:06 +0100

    added tests for useShallowEqualMemo

--- src/__tests__/hooks/useShallowEqualMemo.test.js
+++ src/__tests__/hooks/useShallowEqualMemo.test.js
@@ -1,1 +1,28 @@
-test.todo("useShallowEqualMemo");
+import { renderHook } from "@testing-library/react";
+import useShallowEqualMemo from "../../hooks/useShallowEqualMemo";
+
+describe("useShallowEqualMemo", () => {
+  const setUp = (val) => renderHook(() => useShallowEqualMemo(val));
+  test("returns the value passed in on mount", () => {
+    const { result } = setUp("Test");
+    expect(result.current).toBe("Test");
+  });
+
+  test("returns the given value if not shallowly equal to the previous", () => {
+    const initial = { Test: "yes" };
+    const { result, rerender } = setUp(initial);
+    expect(result.current).toStrictEqual(initial);
+    initial.equal = "Yes";
+    rerender();
+    expect(result.current).toStrictEqual({ equal: "Yes", Test: "yes" });
+  });
+
+  test("returns the previous value if the next compares shallowly equal", () => {
+    const initial = { Test: ["yes"] };
+    const { result, rerender } = setUp(initial);
+    expect(result.current).toStrictEqual(initial);
+    initial.equal = "No";
+    rerender();
+    expect(result.current).toStrictEqual(initial);
+  });
+});
