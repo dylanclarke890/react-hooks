11cbf3fcb9f9f6d48a12fd5b73ec3aa9b9aa1d4b
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-08 18:02:50 +0100

    added useWindowRef tests

--- src/__tests__/hooks/browser/useWindowRef.test.js
+++ src/__tests__/hooks/browser/useWindowRef.test.js
@@ -1,1 +1,17 @@
-test.todo("useWindowRef");
+import { renderHook } from "@testing-library/react";
+import useWindowRef from "../../../hooks/browser/useWindowRef";
+
+let windowSpy;
+
+beforeEach(() => {
+  windowSpy = jest.spyOn(window, "window", "get");
+});
+
+afterEach(() => {
+  windowSpy.mockRestore();
+});
+
+test("useWindowRef returns a ref to the window", () => {
+  renderHook(() => useWindowRef());
+  expect(windowSpy).toBeCalled();
+});
