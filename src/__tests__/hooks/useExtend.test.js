c82bf60da0689166db7ba582406b4780438fc9ed
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 13:36:25 +0100

    added tests for useExtend

--- src/__tests__/hooks/useExtend.test.js
+++ src/__tests__/hooks/useExtend.test.js
@@ -1,1 +1,25 @@
-test.todo("useExtend");
+import { renderHook } from "@testing-library/react-hooks";
+import useExtend from "../../hooks/useExtend";
+
+describe("useExtend", () => {
+  const setUp = (deps, initialState = {}) =>
+    renderHook(() => useExtend(initialState, deps || []));
+  test("returns initial state on mount", () => {
+    const { result } = setUp();
+    expect(result.current).toStrictEqual({});
+  });
+
+  test("allows state to be extended if obj passed in deps", () => {
+    const { result } = setUp([{ test: true }]);
+    expect(result.current).toStrictEqual({ test: true });
+  });
+
+  test("updates the state when a dependancy changes", () => {
+    let dep = ["someVal"];
+    const { result } = setUp([dep], () => dep);
+    expect(result.current).toStrictEqual(["someVal"]);
+
+    dep = ["newVal"];
+    expect(result.current).toStrictEqual(["someVal"]);
+  });
+});
--- src/hooks/useExtend.js
+++ src/hooks/useExtend.js
@@ -1,10 +1,10 @@
 import { useMemo } from "react";
-import { extend, isPlainObject } from "dc-javascript-utils";
+import { extend, isObject } from "dc-javascript-utils";
 import useShallowEqualMemo from "./useShallowEqualMemo";
 
 /**
  * Hook to extend an object with an array of source objects.
- * @param {Object|Function} destination Destination object or a function returning a destination
+ * @param {Object|Function} target Destination object or a function returning a destination
  * object.
  * @param {Array} deps An array which defines the dependecies of the hook as well the source
  * objects to use to extend the destination object. If the nth element of this array is an
@@ -12,16 +12,16 @@ import useShallowEqualMemo from "./useShallowEqualMemo";
  * "destination", as well as used as a dep. If the nth element of this array is not an object,
  * it will only be used as a dep.
  * @param {Object} [extendOptions] The options for the extension.
+ * @param {boolean} [extendOptions.extendArrays] If true, will also extend arrays.
  * @return {Object} The extended destination object.
  */
-export default function useExtend(destination, deps, extendOptions = {}) {
+export default function useExtend(target, deps = [], extendOptions = {}) {
   const obj = useMemo(
-    typeof destination === "function" ? destination : () => destination,
+    typeof target === "function" ? target : () => target,
     deps
   );
   extendOptions = useShallowEqualMemo(extendOptions);
-  const extendFn = () =>
-    extend(obj, ...deps.filter(isPlainObject), [extendOptions]);
-  const finalObj = useMemo(extendFn, [...deps, extendOptions]);
-  return finalObj;
+  const extendFn = () => extend(obj, ...deps.filter(isObject), [extendOptions]);
+  const extendedObj = useMemo(extendFn, [...deps, extendOptions]);
+  return extendedObj;
 }
