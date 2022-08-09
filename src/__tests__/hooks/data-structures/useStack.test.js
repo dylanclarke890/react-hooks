56fe458afc0305c59d40a53305aea05a8b2b24b6
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 23:48:46 +0100

    added useStack tests

--- src/__tests__/hooks/data-structures/useStack.test.js
+++ src/__tests__/hooks/data-structures/useStack.test.js
@@ -1,1 +1,30 @@
-test.todo("useStack");
+import { renderHook } from "@testing-library/react-hooks";
+import { act } from "react-dom/test-utils";
+import useStack from "../../../hooks/data-structures/useStack";
+
+describe("useStack", () => {
+  const setUp = () => renderHook(() => useStack([]));
+  test("returns default state", () => {
+    const { result } = setUp();
+    const [arr, push, pop] = result.current;
+    expect(arr).toStrictEqual([]);
+  });
+
+  test("pushes an item to the stack", () => {
+    const { result } = setUp();
+    const [arr, push, pop] = result.current;
+    expect(arr).toStrictEqual([]);
+    push("test");
+    expect(result.current[0]).toStrictEqual(["test"]);
+  });
+
+  test("pops an item from the stack", () => {
+    const { result } = setUp();
+    const [arr, push, pop] = result.current;
+    expect(arr).toStrictEqual([]);
+    push("test");
+    expect(result.current[0]).toStrictEqual(["test"]);
+    expect(result.current[2]()).toStrictEqual("test");
+    expect(result.current[0]).toStrictEqual([]);
+  });
+});
