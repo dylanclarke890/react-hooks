37bc0c40f90f8e6a8f89ccaecb47aa20bd63a0f5
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 23:52:05 +0100

    added tests for useQueue

--- src/__tests__/hooks/data-structures/useQueue.test.js
+++ src/__tests__/hooks/data-structures/useQueue.test.js
@@ -1,1 +1,30 @@
-test.todo("useQueue");
+import { renderHook } from "@testing-library/react-hooks";
+import useQueue from "../../../hooks/data-structures/useQueue";
+
+describe("useQueue", () => {
+  const setUp = () => renderHook(() => useQueue([]));
+  test("returns default state", () => {
+    const { result } = setUp();
+    expect(result.current[0]).toStrictEqual([]);
+  });
+
+  test("adds item to queue", () => {
+    const { result } = setUp();
+    expect(result.current[0]).toStrictEqual([]);
+    result.current[1]("test");
+    expect(result.current[0]).toStrictEqual(["test"]);
+    result.current[1]("ing");
+    expect(result.current[0]).toStrictEqual(["test", "ing"]);
+  });
+
+  test("removes item from queue", () => {
+    const { result } = setUp();
+    expect(result.current[0]).toStrictEqual([]);
+    result.current[1]("test");
+    expect(result.current[0]).toStrictEqual(["test"]);
+    result.current[1]("ing");
+    expect(result.current[0]).toStrictEqual(["test", "ing"]);
+    expect(result.current[2]()).toBe("test");
+    expect(result.current[0]).toStrictEqual(["ing"]);
+  });
+});
