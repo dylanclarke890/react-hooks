9783d06f5ff298d52d493b319e51a59b4dfb76ff
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 22:15:41 +0100

    added useFactory tests

--- src/__tests__/hooks/useFactory.test.js
+++ src/__tests__/hooks/useFactory.test.js
@@ -1,1 +1,16 @@
-test.todo("useFactory");
+import { renderHook } from "@testing-library/react-hooks";
+import useFactory from "../../hooks/useFactory";
+
+describe("useFactory", () => {
+  const setUp = (tuples, deps) => renderHook(() => useFactory(tuples, deps));
+
+  test("returns factory state on mount", () => {
+    const { result } = setUp(() => [[true, () => true]]);
+    expect(result.current()).toBe(true);
+  });
+
+  test("returns default state on mount", () => {
+    const { result } = setUp(() => [[false, () => true], () => true]);
+    expect(result.current()).toBe(true);
+  });
+});
