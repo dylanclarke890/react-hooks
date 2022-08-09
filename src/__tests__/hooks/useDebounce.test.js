06305d82c342332415745a3c7c5f00580c63c18a
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 21:53:50 +0100

    added tests for useDebounce

--- src/__tests__/hooks/useDebounce.test.js
+++ src/__tests__/hooks/useDebounce.test.js
@@ -1,1 +1,14 @@
-test.todo("useDebounce");
+import { renderHook } from "@testing-library/react-hooks";
+import useDebounce from "../../hooks/useDebounce";
+
+describe("useDebounce", () => {
+  const setUp = (fn, delay) => renderHook(() => useDebounce(fn, delay));
+  test("executes just once", () => {
+    jest.useFakeTimers();
+    const func = jest.fn(() => true);
+    const { result } = setUp(func, 1000);
+    for (let i = 0; i < 100; i++) result.current();
+    jest.runAllTimers();
+    expect(func).toBeCalledTimes(1);
+  });
+});
