98ec2e08d1c9aa9cf4ed498e456c8b43510a8cfd
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-10 00:02:47 +0100

    added test for useCallbackRef

--- src/__tests__/hooks/callback/useCallbackRef.test.js
+++ src/__tests__/hooks/callback/useCallbackRef.test.js
@@ -1,1 +1,9 @@
-test.todo("useCallbackRef");
+import { renderHook } from "@testing-library/react-hooks";
+import useCallbackRef from "../../../hooks/callback/useCallbackRef";
+describe("useCallbackRef", () => {
+  test("returns ref to callback", () => {
+    const mock = jest.fn(() => true);
+    const { result } = renderHook(() => useCallbackRef(mock));
+    expect(result.current.current()).toBe(true);
+  });
+});
