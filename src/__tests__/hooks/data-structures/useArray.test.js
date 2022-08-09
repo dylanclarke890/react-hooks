93c1008dba8b7a2dfc79796a82a5365cfb9055f4
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-09 23:43:11 +0100

    added test for useArray

--- src/__tests__/hooks/data-structures/useArray.test.js
+++ src/__tests__/hooks/data-structures/useArray.test.js
@@ -1,1 +1,11 @@
-test.todo("useArray");
+import useArray from "../../../hooks/data-structures/useArray";
+import { renderHook } from "@testing-library/react-hooks";
+
+describe("useArray", () => {
+  const setUp = () => renderHook(() => useArray());
+  test("returns default state", () => {
+    const { result } = setUp();
+    expect(result.current).toBeUndefined();
+  });
+});
+
--- src/hooks/data-structures/useArray.js
+++ src/hooks/data-structures/useArray.js
@@ -1,19 +1,15 @@
 import useFactory from "../useFactory";
-import { ImmutableLinkedOrderedMap } from "dc-javascript-utils";
 
 /**
- * Hook returning an array for a given a data structure.
- * @param {Array|ImmutableLinkedOrderedMap} dataStructure A data structure.
+ * Hook returning an array for a given data structure.
+ * @param {Array} dataStructure A data structure.
  * @return {Array} An array containing the values of the data structure.
  * If an array is given as the data structure, the same array will be returned by this hook.
  */
 export default function useArray(dataStructure) {
   const arrayFactoryFn = useFactory(
     () => [
-      [
-        ImmutableLinkedOrderedMap.isMap(dataStructure),
-        () => dataStructure.values(),
-      ],
+      [dataStructure instanceof Map, () => dataStructure.values()],
       () => dataStructure,
     ],
     [dataStructure]
