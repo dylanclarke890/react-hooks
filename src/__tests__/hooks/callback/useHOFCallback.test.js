d2f218b4c2d45b9216f38421a44d568a9ccade19
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-10 11:08:41 +0100

    added useHOFCallback tests

--- src/__tests__/hooks/callback/useHOFCallback.test.js
+++ src/__tests__/hooks/callback/useHOFCallback.test.js
@@ -1,1 +1,27 @@
-test.todo("useHOFCallback");
+import { renderHook } from "@testing-library/react-hooks";
+import useHOFCallback from "../../../hooks/callback/useHOFCallback";
+
+describe("useHOFCallback", () => {
+  const setUp = (fn, deps) => renderHook(() => useHOFCallback(fn, deps));
+
+  test("returns function to create functions with", () => {
+    let mockVal = ["yes"];
+    const mock = jest.fn(() => mockVal);
+    const { result } = setUp(mock, [mockVal]);
+    expect(typeof result.current).toBe("function");
+    const res = result.current();
+    expect(res()).toStrictEqual(["yes"]);
+  });
+
+  test("update function if deps change", () => {
+    let mockVal = ["yes"];
+    const mock = jest.fn(() => mockVal);
+    const { result } = setUp(mock, [mockVal]);
+    expect(typeof result.current).toBe("function");
+    const res = result.current();
+    expect(res()).toStrictEqual(["yes"]);
+    mockVal = ["no"];
+    const change = result.current();
+    expect(change()).toStrictEqual(["no"]);
+  });
+});
