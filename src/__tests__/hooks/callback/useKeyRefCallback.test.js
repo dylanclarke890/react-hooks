49e6ed1668d1219b0ee9364109d8ad4d9251b7c3
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 03:46:30 +0100

    added test for keyrefcallback

--- src/__tests__/hooks/callback/useKeyRefCallback.test.js
+++ src/__tests__/hooks/callback/useKeyRefCallback.test.js
@@ -1,1 +1,11 @@
-test.todo("useKeyRefCallback");
+import { renderHook } from "@testing-library/react";
+import useKeyRefCallback from "../../../hooks/callback/useKeyRefCallback";
+
+describe("useKeyRefCallback", () => {
+  const setUp = () => renderHook(() => useKeyRefCallback());
+
+  test("returns default state", () => {
+    const { result } = setUp();
+    expect(result.current("test")).toEqual({ current: null });
+  });
+});
