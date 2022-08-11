18577f17b5f262393ae3f9035cfb279c2908b6f7
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 21:58:59 +0100

    added test for usePOJOMemoState

--- src/__tests__/hooks/state/usePOJOMemoState.test.js
+++ src/__tests__/hooks/state/usePOJOMemoState.test.js
@@ -1,1 +1,10 @@
-test.todo("usePOJOMemoState");
+import { renderHook } from "@testing-library/react";
+import usePOJOMemoState from "../../../hooks/state/usePOJOMemoState";
+
+describe("usePOJOMemoState", () => {
+  const setUp = (initState) => renderHook(() => usePOJOMemoState(initState));
+  test("returns default state", () => {
+    const { result } = setUp({ test: true });
+    expect(result.current[0]).toStrictEqual({ test: true });
+  });
+});
