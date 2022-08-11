b3ef4d3820495b08c9114fb549351338e83eed7b
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 18:56:37 +0100

    added test for useSyncRefsCallback

--- src/__tests__/hooks/callback/useSyncRefsCallback.test.js
+++ src/__tests__/hooks/callback/useSyncRefsCallback.test.js
@@ -1,1 +1,10 @@
-test.todo("useSyncRefsCallback");
+import { renderHook } from "@testing-library/react";
+import useSyncRefsCallback from "../../../hooks/callback/useSyncRefsCallback";
+
+describe("useSyncRefsCallback", () => {
+  const setUp = (refs) => renderHook(() => useSyncRefsCallback(...refs));
+  test("returns default state", () => {
+    const { result } = setUp([]);
+    expect(result.current()).toBe(undefined);
+  });
+});
