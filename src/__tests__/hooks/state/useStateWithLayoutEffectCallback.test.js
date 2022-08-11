9a0dd5b2f0e05af4a8a29db2d2c7073ed1a1d8d3
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 21:55:01 +0100

    added test for useStateWithLayoutEffectCallback

--- src/__tests__/hooks/state/useStateWithLayoutEffectCallback.test.js
+++ src/__tests__/hooks/state/useStateWithLayoutEffectCallback.test.js
@@ -1,1 +1,12 @@
-test.todo("useStateWithLayoutEffectCallback");
+import { renderHook } from "@testing-library/react";
+import useStateWithLayoutEffectCallback from "../../../hooks/state/useStateWithLayoutEffectCallback";
+
+describe("useStateWithLayoutEffectCallback", () => {
+  const setUp = (initState) =>
+    renderHook(() => useStateWithLayoutEffectCallback(initState));
+
+  test("returns default state", () => {
+    const { result } = setUp("test");
+    expect(result.current[0]).toBe("test");
+  });
+});
