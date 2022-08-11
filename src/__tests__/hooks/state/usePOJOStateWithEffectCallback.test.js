cb2c51505a22fdb00822ddd7c97b8f52e3147ab6
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 22:08:18 +0100

    added test for usePOJOStateWithEffectCallback

--- src/__tests__/hooks/state/usePOJOStateWithEffectCallback.test.js
+++ src/__tests__/hooks/state/usePOJOStateWithEffectCallback.test.js
@@ -1,1 +1,11 @@
-test.todo("usePOJOStateWithEffectCallback");
+import { renderHook } from "@testing-library/react";
+import usePOJOStateWithEffectCallback from "../../../hooks/state/usePOJOStateWithEffectCallback";
+
+describe("usePOJOStateWithEffectCallback", () => {
+  const setUp = (initState) =>
+    renderHook(() => usePOJOStateWithEffectCallback(initState));
+  test("returns default state", () => {
+    const { result } = setUp({ test: true });
+    expect(result.current[0]).toStrictEqual({ test: true });
+  });
+});
