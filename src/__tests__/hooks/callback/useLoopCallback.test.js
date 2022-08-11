1972f486cb5ae3c28f27e3d93832794d7e30dd3d
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 03:55:08 +0100

    added test for useLoopCallback

--- src/__tests__/hooks/callback/useLoopCallback.test.js
+++ src/__tests__/hooks/callback/useLoopCallback.test.js
@@ -1,1 +1,11 @@
-test.todo("useLoopCallback");
+import { renderHook } from "@testing-library/react";
+import useLoopCallback from "../../../hooks/callback/useLoopCallback";
+
+describe("useLoopCallback", () => {
+  const setUp = (arr) => renderHook(() => useLoopCallback(arr));
+
+  test("returns default state", () => {
+    const { result } = setUp(["test"]);
+    expect(result.current((v) => v + "ing")).toStrictEqual(["testing"]);
+  });
+});
--- src/hooks/callback/useLoopCallback.js
+++ src/hooks/callback/useLoopCallback.js
@@ -1,21 +1,14 @@
 import useFactory from "../useFactory";
-import { ImmutableLinkedOrderedMap } from "dc-javascript-utils";
 
 /**
  * Hook generating a callback to easily loop through a data structure.
- * @param {Array|ImmutableLinkedOrderedMap} dataStructure A data structure.
+ * @param {Array} dataStructure A data structure.
  * @return {(fn: (value: *) => *) => Array} A function which receives a callback to loop through
  * the values of the data structure returning an array with the mapped values.
  */
 export default function useLoopCallback(dataStructure) {
   const loopCallback = useFactory(
-    () => [
-      [
-        ImmutableLinkedOrderedMap.isMap(dataStructure),
-        (fn) => dataStructure.map(fn),
-      ],
-      (fn) => dataStructure.map(fn),
-    ],
+    () => [(fn) => dataStructure.map(fn)],
     [dataStructure]
   );
   return loopCallback;
