f2651badbeea68a108bf96a5a6d2bb20f4bd2946
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 17:18:36 +0100

    added test for useMemoCallback

--- src/__tests__/hooks/callback/useMemoCallback.test.js
+++ src/__tests__/hooks/callback/useMemoCallback.test.js
@@ -1,1 +1,13 @@
-test.todo("useMemoCallback");
+import { renderHook } from "@testing-library/react";
+import useMemoCallback from "../../../hooks/callback/useMemoCallback";
+
+describe("useMemoCallback", () => {
+  const setUp = (fn) => renderHook(() => useMemoCallback(fn));
+
+  test("returns a callback", () => {
+    let fnVal = true;
+    const mock = jest.fn(() => fnVal);
+    const { result } = setUp(mock);
+    expect(result.current()).toBe(true);
+  });
+});
