a43e47fa6a89d95180d7cfe94f515c728af8f9c2
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-11 22:09:26 +0100

    added test for usePOJOStateWithLayoutEffect

--- src/__tests__/hooks/state/usePOJOStateWithLayoutEffect.test.js
+++ src/__tests__/hooks/state/usePOJOStateWithLayoutEffect.test.js
@@ -1,1 +1,11 @@
-test.todo("usePOJOStateWithLayoutEffect");
+import { renderHook } from "@testing-library/react";
+import usePOJOStateWithLayoutEffect from "../../../hooks/state/usePOJOStateWithLayoutEffect";
+
+describe("usePOJOStateWithLayoutEffect", () => {
+  const setUp = (initState) =>
+    renderHook(() => usePOJOStateWithLayoutEffect(initState));
+  test("returns default state", () => {
+    const { result } = setUp({ test: true });
+    expect(result.current[0]).toStrictEqual({ test: true });
+  });
+});
